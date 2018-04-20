4d033c8ca1c82a5122d0ef7902b68a694665fabf
Mohit Singh Negi <mohitnegi724@gmail.com>
2018-04-20 07:49:01 +0530

    Add Input and Show Data without Reloading the Client Browser

--- public/javascript/shortener.js
+++ public/javascript/shortener.js
@@ -1,28 +1,10 @@
-// function getAllData(){
-//   fetch('/shortlinks')
-//   .then((res) => res.json())
-//   .then((data) => {
-//     let URLS = '';
-//     data.forEach(function(shortURL){
-//       URLS += `
-//       <tr>
-//           <td class="oneFourth" id="OriginalLink"><a href="${shortURL.originalURL}">${shortURL.originalURL}</a></td>
-//           <td class="oneFourth">${shortURL.createdDate}</td>
-//           <td class="ShortLink"><a href="/${shortURL.shortId}" target="_blank" id="mainLink">https://shorturlbymohit.herokuapp.com/${shortURL.shortId}</a><i class="material-icons" id="previewLink">visibility</i></td>
-//           <td class="oneFourth"><i class="material-icons"><a class="delete" href="/${shortURL.shortId}/delete">delete</a></i></td>
-//       </tr>
-//       `
-//     });
-//     document.getElementById('output').innerHTML = URLS;
-//   })
-// }
-
-// getAllData()
+//Getting Data From MongoDb and Sending Them to Client Browser
 
 function getAllData(){
   $.ajax({
-    method: "POST",
-    url : "/ShortLinks", 
+    type: "POST",
+    url : "/ShortLinks",
+    data: $('form').serialize(), 
   })
   .done((data)=>{
     let URLS = '';
@@ -40,5 +22,20 @@ function getAllData(){
   })
   .fail((err)=>{alert(err)})
 }
+getAllData();
 
-getAllData()
+
+//Saving Input in Backend and Frontend without REloading the Browser
+$(function () {
+        $('form').on('submit', function (e) {
+          e.preventDefault();
+          $.ajax({
+            type: 'post',
+            url: '/shortURL',
+            data: $('form').serialize(),
+            success: function () {
+              return getAllData()
+            }
+          });
+        });
+      });
